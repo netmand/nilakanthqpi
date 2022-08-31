97ba5edf1a99a682d308b1e9775822f3d713008f
danielhetorilla <danielhetorilla@DESKTOP-MD668SD>
2022-08-31 10:14:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nilakanthapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iterations = "1";
+            int interationi = 1;
+            pi pi;
+            while (interationi != 0)
+            {
+                Console.Write("\tEnter Number of Iterations to run:");
+                iterations = Console.ReadLine();
+                if (int.TryParse(iterations, out interationi))
+                {
+                    pi = new pi(interationi);
+                    Console.Write(pi.CalcPi().ToString());
+                }
+            }
+        }
+    }  
+
+}
---
+++ pi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nilakanthapi
+{
+    internal class pi
+    {
+        private int iteration;
+        public pi()
+        {
+            iteration = 0;
+        }
+        public pi(int iteration)
+        {
+            this.iteration = iteration;
+        }
+        public double CalcPi()
+        {
+            double value = 3;
+            double bookmark = 2;
+            for(int i = 0; i < this.iteration; i++)
+            {
+                value += (4 / (bookmark * (bookmark + 1) * (bookmark + 2)) - 4 / ((bookmark + 2) * (bookmark + 3) * (bookmark + 4)));
+                bookmark += 4;
+            }
+            return value;
+        }
+    }
+}
